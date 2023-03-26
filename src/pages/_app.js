c6351033e210ec1b8e0a6a595f503dc413a25e7b
Rhfialyndra <l>
2023-03-26 11:45:04 +0700

    fix: layouting

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import UserAuthContextProvider from "@components/contexts/UserAuthContext";
+import Layout from "@components/layout/Layout";
 
 function GoBlog({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserAuthContextProvider>
+      
+    <Layout>
+
+      <Component {...pageProps} />
+    </Layout>
+      
+    </UserAuthContextProvider>
+  );
 }
 
 export default GoBlog;
