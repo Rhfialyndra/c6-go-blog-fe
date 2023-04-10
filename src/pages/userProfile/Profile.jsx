cbb99e55742701ba36c45d8f5ac0dd13f2611b47
unknown <diah.afia@ui.ac.id>
2023-04-10 12:44:22 +0700

    user profile

--- src/pages/userProfile/Profile.jsx
+++ src/pages/userProfile/Profile.jsx
@@ -26,21 +26,6 @@ const profile = () => {
                   <h1 className="pl-3">X Post</h1>
                 </div>
               </div>
-              <div className="flex pl-5 pt-3 pb-3 pr-5">
-                {/*<input*/}
-                {/*  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"*/}
-                {/*  id="username" type="text" placeholder="Email"></input>*/}
-                <div class="flex flex-col w-full h-full">
-                <div className="w-full h-full mx-auto  border border-gray-300 rounded-lg bg-gray-100">
-                {/*  <p>Ini adalah kotak teks</p>*/}
-                </div>
-                </div>
-                {/*<h1 className="font-bold pt-2">Full Name</h1>*/}
-                {/*<div className="shadow appearance-none border rounded w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline">*/}
-                {/*<div class="border rounded p-2 bg-white w-full">*/}
-                {/*  <p>Ini adalah kotak teks</p>*/}
-                {/*</div>*/}
-              </div>
           </div>
         </div>
       </div>
